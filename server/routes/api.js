30dc786b6b64d1fd3ff7fbffab6aafdf5109080b
Hussain Omer <m.hussainomer03@gmail.com>
2024-11-23 20:13:20 -0500

    refactor db connection

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,33 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { MongoClient, ObjectId } = require('mongodb');
+const { ObjectId } = require('mongodb');
 const nodemailer = require('nodemailer');
 const authenticateToken = require('../middleware/auth');
-
-let cachedDb = null;
-
-async function connectToDatabase() {
-  if (cachedDb) {
-    return cachedDb;
-  }
-  const client = await MongoClient.connect(process.env.MONGODB_URI);
-  const db = client.db("usersDB");
-  cachedDb = db;
-  return db;
-}
-
-async function initializeIndexes() {
-  try {
-    const db = await connectToDatabase();
-    const usersCollection = db.collection('users');
-    await usersCollection.dropIndexes();
-    await usersCollection.createIndex({ googleId: 1 }, { unique: true });
-  } catch (error) {
-    console.error('Error initializing database indexes:', error);
-  }
-}
-
-initializeIndexes();
+const { connectToDatabase } = require('../db');
 
 // User routes
 router.post('/users', async (req, res) => {
