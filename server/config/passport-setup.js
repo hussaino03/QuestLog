3ee8193daa3bf75a6c2601e212c4b75eee349080
Hussain Omer <m.hussainomer03@gmail.com>
2024-11-26 22:54:58 -0500

    Migrate to Passport

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -20,7 +20,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.CALLBACK,
+  callbackURL: '/api/auth/google/callback',
   proxy: true 
   },
   async (_accessToken, _refreshToken, profile, done) => {
