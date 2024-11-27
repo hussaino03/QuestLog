d6588788d145842f22f95a6d06ebe76e4a2beac5
hussaino03 <m.hussainomer03@gmail.com>
2024-11-27 02:42:19 -0500

    Delete server/routes/Leaderboard directory

--- server/routes/Leaderboard/Leaderboard.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getLeaderboard } = require('./leaderboard.controller');
-const { authenticateToken } = require('../../middleware/auth');
-
-router.get('/', authenticateToken, getLeaderboard);
-
-module.exports = router;
