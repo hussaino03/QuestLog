51775db26f42254e916b1bc88a5094300e490960
Hussain Omer <m.hussainomer03@gmail.com>
2024-11-28 00:24:57 -0500

    analytics

--- scripts/getUserCount.js
+++ scripts/getUserCount.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from '../server/db';
+import { connectToDatabase } from '../server/db.js';
 import { promises as fs } from 'fs';
 import { join } from 'path';
 import { fileURLToPath } from 'url';
